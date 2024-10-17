0539dfbf7d3257c468c1f74d93e4afa35eae1b08
Nic <hsnicco@gmail.com>
2024-10-18 01:52:39 +0200

    test lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { ImageProps } from './types';
-import GalleryImage from './components/GalleryImage';
+// import { ImageProps } from './types';
+// import GalleryImage from './components/GalleryImage';
 
 export default async function Home() {
 
